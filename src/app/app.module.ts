983f0a18bd1911898235e4466b7d44d33e968e53
Agaesseremi <120452380+Agaesseremi@users.noreply.github.com>
2023-07-12 09:35:04 +0200

    Revert "fin 11 juillet"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ConfigAlpineBarComponent } from './components/config-alpine-bar/config-alpine-bar.component';
-import { ChoiceBarComponent } from './components/choice-bar/choice-bar.component';
-import { ChoiceCardComponent } from './components/choice-card/choice-card.component';
 
 
 @NgModule({
@@ -15,8 +13,6 @@ import { ChoiceCardComponent } from './components/choice-card/choice-card.compon
     AppComponent,
     NavBarComponent,
     ConfigAlpineBarComponent,
-    ChoiceBarComponent,
-    ChoiceCardComponent,
 
   ],
   imports: [
