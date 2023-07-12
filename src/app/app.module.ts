a1a67a3d34e0cad4de2cbcbd34059cdf6a6df3c8
AgaesseRemi <remiagaesse1@gmail.com>
2023-07-12 09:33:30 +0200

    12/07 morning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ConfigAlpineBarComponent } from './components/config-alpine-bar/config-alpine-bar.component';
+import { EffectsModule } from '@ngrx/effects';
+import { ViewImageComperatorComponent } from './components/view-image-comperator/view-image-comperator.component'
+import { Reducer } from './reducer/reducer';
+import { ButtonChoiceComponent } from './components/button-choice/button-choice.component';
 
 
 @NgModule({
@@ -13,12 +17,14 @@ import { ConfigAlpineBarComponent } from './components/config-alpine-bar/config-
     AppComponent,
     NavBarComponent,
     ConfigAlpineBarComponent,
+    ViewImageComperatorComponent,
+    ButtonChoiceComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({ alpine: Reducer }, {}),
   ],
   providers: [],
   bootstrap: [AppComponent]
